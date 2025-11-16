84b5c82e07c36b35d280f1bebe910e7c38c4188c
Gideon Isa <gideon@insightful3d.com>
2025-11-16 01:25:09 +0100

    implement content video streaming

---
+++ Lms.ContentDelivery.Application/Abstractions/ICourseVideoRespository.cs
@@ -0,0 +1,9 @@
+﻿using Lms.ContentDelivery.Application.Models;
+
+namespace Lms.ContentDelivery.Application.Abstractions
+{
+    public interface ICourseVideoRespository
+    {
+        Task<VideoMetadataModel?> GetVideoMetadataByIdAsync(Guid videoId, CancellationToken cancellationToken);
+    }
+}
---
+++ Lms.ContentDelivery.Application/Features/Queries/ContentDelivery/GetVideoResourceByIdQuery.cs
@@ -0,0 +1,12 @@
+﻿using Lms.ContentDelivery.Application.Features.Queries.Dto;
+using Lms.Shared.Abstractions.Interfaces.Request;
+using Lms.SharedKernel.Common.Wrappers;
+
+namespace Lms.ContentDelivery.Application.Features.Queries.ContentDelivery
+{
+    public class GetVideoResourceByIdQuery : ICustomRequest<IResponseWrapper<StreamLessonVideoResponse>>
+    {
+        public Guid VideoId { get; set; }
+
+    }
+}
